08e21d0e30f575c03f9afd7cf3fa999dfd410e3f
jennhsu9 <jennhsu9@gmail.com>
2013-11-02 17:29:52 -0700

    inital commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Puzzledragons::Application.config.secret_key_base = 'b76a217c30aaf8b7b2c6a8e4373f151c5592d82d2f94579b1a9217b232d979a8b9dd6bf4fb676de87324a3a18e6f5bbc2a7937fc9776e08be42102b877b27e50'
